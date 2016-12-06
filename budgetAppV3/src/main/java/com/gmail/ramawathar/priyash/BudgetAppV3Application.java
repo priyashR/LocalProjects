9b840ce9d26388951aa33baa4f9655e3d13e07f0
Priyash.Ramawthar <Priyash.Ramawthar@Priyashr-JHB.AdaptIT.co.za>
2016-12-06 12:22:16 +0200

    budget app version3

---
+++ budgetAppV3/src/main/java/com/gmail/ramawathar/priyash/BudgetAppV3Application.java
@@ -0,0 +1,13 @@
+package com.gmail.ramawathar.priyash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BudgetAppV3Application  extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAppV3Application.class, args);
+	}
+}
---
+++ budgetAppV3/src/test/java/com/gmail/ramawathar/priyash/BudgetAppV3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.ramawathar.priyash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BudgetAppV3Application.class)
+public class BudgetAppV3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
