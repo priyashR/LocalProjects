01bd9e064da570a67ad5f7c9d0b99977d47eae4a
Priyash.Ramawthar <Priyash.Ramawthar@Priyashr-JHB.AdaptIT.co.za>
2016-12-08 00:25:38 +0200

    Revert "JPA + Boot working"
    This reverts commit 399a375edaa7fdcf4375237955910585a21bb5b0.

--- budgetAppV4_1/src/main/java/com/gmail/ramawthar/priyash/repo/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.ramawthar.priyash.repo;
-
-import com.gmail.ramawthar.priyash.model.*;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GameRepository extends CrudRepository<Game, Integer> {
-
-}
