8f5b561459e9ce4eb2276d4364357151c6f2ed73
Priyash.Ramawthar <Priyash.Ramawthar@Priyashr-JHB.AdaptIT.co.za>
2016-12-08 00:34:38 +0200

    restored

---
+++ budgetAppV4_1/src/main/java/com/gmail/ramawthar/priyash/repo/GameRepository.java
@@ -0,0 +1,8 @@
+package com.gmail.ramawthar.priyash.repo;
+
+import com.gmail.ramawthar.priyash.model.*;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameRepository extends CrudRepository<Game, Integer> {
+
+}
---
+++ budgetAppV4_1/src/test/java/com/gmail/ramawthar/priyash/BudgetAppV41ApplicationTests.java
@@ -0,0 +1,51 @@
+package com.gmail.ramawthar.priyash;
+
+import com.gmail.ramawthar.priyash.model.Game;
+import com.gmail.ramawthar.priyash.repo.GameRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+import com.gmail.ramawthar.priyash.BudgetAppV41Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BudgetAppV41Application.class)
+public class BudgetAppV41ApplicationTests {
+
+	 @Autowired
+	 GameRepository gameRepository;
+	 
+	 @Before
+	 public void setUp() throws Exception {
+	  gameRepository.deleteAll();
+	  Game pandemic = new Game("Pandemic", "Co-op game for  ");
+	  Game werewolf = new Game("Werewolf", "You must find  ");
+	  Game camelUp = new Game("Camel Up", "A high stak ");
+	  gameRepository.save(pandemic);
+	  gameRepository.save(werewolf);
+	  gameRepository.save(camelUp);
+	 }	 
+	 
+	 @Test
+	 public void testLoadGames() {
+
+		  gameRepository.deleteAll();
+		  Game pandemic = new Game("Pandemic", "Co-op game  ");
+		  Game werewolf = new Game("Werewolf", "You must f ");
+		  Game camelUp = new Game("Camel Up", "A high sta ");
+		  gameRepository.save(pandemic);
+		  gameRepository.save(werewolf);
+		  gameRepository.save(camelUp);
+	     List<Game> games = (ArrayList<Game>) gameRepository.findAll();
+	     assertEquals("Did not get all games", 3, games.size());
+	 }
+
+
+}
