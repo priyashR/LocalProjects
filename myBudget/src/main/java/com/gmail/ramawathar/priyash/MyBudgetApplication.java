e37ebf5fd3d2d18f7f05fb5ad684d63bc59e80a9
priyashR <priyash.ramawthar@gmail.com>
2017-05-10 00:17:55 +0200

    new app

---
+++ myBudget/src/main/java/com/gmail/ramawathar/priyash/MyBudgetApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.ramawathar.priyash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetApplication.class, args);
+	}
+}
