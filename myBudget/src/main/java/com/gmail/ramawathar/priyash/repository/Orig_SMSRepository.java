113add66f6dd7192ffc84efdd84febd5116e92b5
priyashR <priyash.ramawthar@gmail.com>
2017-07-20 13:35:27 +0200

    added the repository

---
+++ myBudget/src/main/java/com/gmail/ramawathar/priyash/repository/Orig_SMSRepository.java
@@ -0,0 +1,9 @@
+package com.gmail.ramawathar.priyash.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gmail.ramawathar.priyash.domain.Orig_SMS;
+
+public interface Orig_SMSRepository extends CrudRepository<Orig_SMS, Long> {
+
+}
