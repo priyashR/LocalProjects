0b7f93b5e2996a624aeef77174d7ed45f632dfc9
priyashR <priyash.ramawthar@gmail.com>
2017-07-21 14:45:18 +0200

    adding swagger

--- myBudget/src/main/java/com/gmail/ramawathar/priyash/MyBudgetApplication.java
+++ myBudget/src/main/java/com/gmail/ramawathar/priyash/MyBudgetApplication.java
@@ -3,7 +3,10 @@ package com.gmail.ramawathar.priyash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
 @SpringBootApplication
+@EnableSwagger 
 public class MyBudgetApplication {
 
 	public static void main(String[] args) {
