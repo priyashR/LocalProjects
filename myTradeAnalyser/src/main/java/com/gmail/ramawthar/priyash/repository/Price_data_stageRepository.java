112296b3fa1987c0a0e13906daaedcaebe21c9fc
priyashR <priyash.ramawthar@gmail.com>
2017-10-26 10:15:27 +0200

    added the repository

---
+++ myTradeAnalyser/src/main/java/com/gmail/ramawthar/priyash/repository/Price_data_stageRepository.java
@@ -0,0 +1,9 @@
+package com.gmail.ramawthar.priyash.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gmail.ramawthar.priyash.domain.Price_data_stage;
+
+public interface Price_data_stageRepository extends CrudRepository<Price_data_stage, Long> {
+
+}
