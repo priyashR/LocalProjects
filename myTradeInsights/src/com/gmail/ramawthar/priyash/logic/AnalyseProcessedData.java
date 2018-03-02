20bacd2e6f62a6acc2002e23493ee377b2e1800e
Priyash <priyash.ramawthar@gmail.com>
2018-03-02 13:26:36 +0200

    refactored

--- myTradeInsights/src/com/gmail/ramawthar/priyash/logic/AnalyseProcessedData.java
+++ myTradeInsights/src/com/gmail/ramawthar/priyash/logic/AnalyseProcessedData.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import com.gmail.ramawthar.priyash.WS.AWS;
 import com.gmail.ramawthar.priyash.responses.ReturnClass;
 
 public class AnalyseProcessedData {
